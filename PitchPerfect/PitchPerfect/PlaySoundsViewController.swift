163b5f034980e0731a6ad707bea273b962ef29af
Martin Rist <mlr-github@martinrist.com>
2017-10-05 22:02:23 +0100

    Wire up all buttons to play sounds

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -12,6 +12,21 @@ class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +37,7 @@ class PlaySoundsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
   
