c5cd521868d8650be9b837502f499afc7de99964
Martin Rist <mlr-github@martinrist.com>
2017-10-07 16:31:06 +0100

    Add code to programmatically construct buttons

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        // Label
+        label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        // Button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc
+    func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
-
 
 }
 
