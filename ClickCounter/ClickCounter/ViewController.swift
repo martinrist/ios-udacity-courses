bdd5d12a91a882507dd3b4f9b61b74fe212e0a89
Martin Rist <mlr-github@martinrist.com>
2017-10-07 16:38:19 +0100

    Convert ClickCounter to use storyboar/d

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -11,30 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label: UILabel!
+    @IBOutlet var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Label
-        label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        
-        // Button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
         
     }
     
-    @objc
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
     }
