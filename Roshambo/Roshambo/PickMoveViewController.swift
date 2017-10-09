351bc2683dff8074c5c9d1c1cffae90034bcb0aa
Martin Rist <mrist@mrist-mbp.local>
2017-10-09 13:56:18 +0100

    Initial storyboard layout and outlets

---
+++ Roshambo/Roshambo/PickMoveViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PickMoveViewController.swift
+//  Roshambo
+//
+//  Created by Martin Rist on 09/10/2017.
+//  Copyright © 2017 Martin Rist. All rights reserved.
+//
+
+import UIKit
+
+class PickMoveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pickPaper(_ sender: Any) {
+    }
+    
+    @IBAction func pickRock(_ sender: Any) {
+    }
+
+    @IBAction func pickScissors(_ sender: Any) {
+    }
+}
+
---
+++ Roshambo/Roshambo/PresentResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PresentResultViewController.swift
+//  Roshambo
+//
+//  Created by Martin Rist on 09/10/2017.
+//  Copyright © 2017 Martin Rist. All rights reserved.
+//
+
+import UIKit
+
+class PresentResultViewController: UIViewController {
+
+    @IBOutlet weak var resultsImage: UIImageView!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
